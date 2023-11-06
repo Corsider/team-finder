b22392eec82262c2b867c0d8e190032405d5abd9
Corsider <e.barsukov05@gmail.com>
2023-11-06 19:42:00 +0300

    new endpoints

--- internal/init.go
+++ internal/init.go
@@ -8,11 +8,20 @@ import (
 var DB *sql.DB
 
 func Init(r *gin.Engine) {
-	r.GET("/setup", setup)
 	r.GET("/users/:id", getUserById)
 	r.GET("/tags/user/:id", getTagsByUser)
 	r.GET("/teams/user/:id", getTeamsByUser)
 	r.GET("/team/:id", getTeamById)
 	r.GET("/tags/team/:id", getTagsByTeam)
 	r.GET("/tags", getAllTags)
+
+	r.GET("/users/team/:id", getUsersByTeam)
+	r.GET("/event/:id", getEventById)
+	r.GET("/teams/event/:id", getTeamsByEvent)
+	r.GET("/tags/event/:id", getTagsByEvent)
+	r.GET("/events", getAllEvents)
+	r.GET("/teams", getAllTeams)
+	r.GET("/global-tags", getAllGlobalTags)
+	r.GET("/tags/global-tag/:id", getTagsByGlobalTag)
+	r.GET("/users", getAllUsers)
 }
--- internal/routes.go
+++ internal/routes.go
@@ -25,9 +25,7 @@ func getUserById(c *gin.Context) {
 		})
 	} else {
 		user.Password = ""
-		c.JSON(200, gin.H{
-			"user": user,
-		})
+		c.JSON(200, user)
 	}
 }
 
@@ -49,9 +47,7 @@ func getTagsByUser(c *gin.Context) {
 			row.Scan(&tag.TagID, &tag.Activity, &tag.GlobalTagID)
 			tags = append(tags, tag)
 		}
-		c.JSON(200, gin.H{
-			"tags": tags,
-		})
+		c.JSON(200, tags)
 	}
 }
 
@@ -69,9 +65,7 @@ func getAllTags(c *gin.Context) {
 			_ = rows.Scan(&tagg.TagID, &tagg.Activity, &tagg.GlobalTagID)
 			tags = append(tags, tagg)
 		}
-		c.JSON(200, gin.H{
-			"tags": tags,
-		})
+		c.JSON(200, tags)
 	}
 }
 
@@ -92,9 +86,7 @@ func getTeamsByUser(c *gin.Context) {
 			row.Scan(&team.TeamID, &team.Name, &team.Rate, &team.Description, &team.Rules, &team.RegDate, &team.Place)
 			teams = append(teams, team)
 		}
-		c.JSON(200, gin.H{
-			"teams": teams,
-		})
+		c.JSON(200, teams)
 	}
 }
 
@@ -109,9 +101,7 @@ func getTeamById(c *gin.Context) {
 			"server": -1,
 		})
 	} else {
-		c.JSON(200, gin.H{
-			"team": team,
-		})
+		c.JSON(200, team)
 	}
 }
 
@@ -132,8 +122,177 @@ func getTagsByTeam(c *gin.Context) {
 			row.Scan(&tag.TagID, &tag.Activity, &tag.GlobalTagID)
 			tags = append(tags, tag)
 		}
-		c.JSON(200, gin.H{
-			"tags": tags,
+		c.JSON(200, tags)
+	}
+}
+
+func getUsersByTeam(c *gin.Context) {
+	id := c.Param("id")
+	rows, err := DB.Query(fmt.Sprintf("select user_id from user_team where team_id=%s", id))
+	if err != nil {
+		log.Println(err)
+		c.JSON(500, gin.H{
+			"server": -1,
 		})
+	} else {
+		usrs := []User{}
+		for rows.Next() {
+			var usr User
+			rows.Scan(&usr.UserId)
+			row := DB.QueryRow(fmt.Sprintf("select * from users where user_id=%s", strconv.Itoa(usr.UserId)))
+			row.Scan(&usr.UserId, &usr.Name, &usr.Nickname, &usr.Rate, &usr.Description, &usr.Login, &usr.Password)
+			usr.Password = ""
+			usrs = append(usrs, usr)
+		}
+		c.JSON(200, usrs)
+	}
+}
+
+func getEventById(c *gin.Context) {
+	id := c.Param("id")
+	row := DB.QueryRow(fmt.Sprintf("select * from events where event_id=%s", id))
+	var event Event
+	err := row.Scan(&event.EventID, &event.Name, &event.Description, &event.Date, &event.Online, &event.MainTheme, &event.Url, &event.CreatorID)
+	if err != nil {
+		log.Println(err)
+		c.JSON(500, gin.H{
+			"server": -1,
+		})
+	} else {
+		c.JSON(200, event)
+	}
+}
+
+func getTeamsByEvent(c *gin.Context) {
+	id := c.Param("id")
+	rows, err := DB.Query(fmt.Sprintf("select team_id from team_event where event_id=%s", id))
+	if err != nil {
+		log.Println(err)
+		c.JSON(500, gin.H{
+			"server": -1,
+		})
+	} else {
+		teams := []Team{}
+		for rows.Next() {
+			var team Team
+			rows.Scan(&team.TeamID)
+			row := DB.QueryRow(fmt.Sprintf("select * from team where team_id=%s", strconv.Itoa(team.TeamID)))
+			row.Scan(&team.TeamID, &team.Name, &team.Rate, &team.Description, &team.Rules, &team.RegDate, &team.Place)
+			teams = append(teams, team)
+		}
+		c.JSON(200, teams)
+	}
+}
+
+func getTagsByEvent(c *gin.Context) {
+	id := c.Param("id")
+	rows, err := DB.Query(fmt.Sprintf("select tag_id from events_tags where event_id=%s", id))
+	if err != nil {
+		log.Println(err)
+		c.JSON(500, gin.H{
+			"server": -1,
+		})
+	} else {
+		tags := []Tag{}
+		for rows.Next() {
+			var tag Tag
+			rows.Scan(&tag.TagID)
+			row := DB.QueryRow(fmt.Sprintf("select * from tag where tag_id=%s", strconv.Itoa(tag.TagID)))
+			row.Scan(&tag.TagID, &tag.Activity, &tag.GlobalTagID)
+			tags = append(tags, tag)
+		}
+		c.JSON(200, tags)
+	}
+}
+
+func getAllEvents(c *gin.Context) {
+	events := []Event{}
+	rows, err := DB.Query(fmt.Sprintf("select * from events"))
+	if err != nil {
+		log.Println(err)
+		c.JSON(500, gin.H{
+			"server": -1,
+		})
+	} else {
+		for rows.Next() {
+			var event Event
+			rows.Scan(&event.EventID, &event.Name, &event.Description, &event.Date, &event.Online, &event.MainTheme, &event.Url, &event.CreatorID)
+			events = append(events, event)
+		}
+		c.JSON(200, events)
+	}
+}
+
+func getAllTeams(c *gin.Context) {
+	teams := []Team{}
+	rows, err := DB.Query(fmt.Sprintf("select * from team"))
+	if err != nil {
+		log.Println(err)
+		c.JSON(500, gin.H{
+			"server": -1,
+		})
+	} else {
+		for rows.Next() {
+			var team Team
+			rows.Scan(&team.TeamID, &team.Name, &team.Rate, &team.Description, &team.Rules, &team.RegDate, &team.Place)
+			teams = append(teams, team)
+		}
+		c.JSON(200, teams)
+	}
+}
+
+func getAllGlobalTags(c *gin.Context) {
+	gtags := []GlobalTag{}
+	rows, err := DB.Query("select * from global_tag")
+	if err != nil {
+		log.Println(err)
+		c.JSON(500, gin.H{
+			"server": -1,
+		})
+	} else {
+		for rows.Next() {
+			var gtag GlobalTag
+			rows.Scan(&gtag.GlobalTagID, &gtag.Category)
+			gtags = append(gtags, gtag)
+		}
+		c.JSON(200, gtags)
+	}
+}
+
+func getTagsByGlobalTag(c *gin.Context) {
+	id := c.Param("id")
+	rows, err := DB.Query(fmt.Sprintf("select * from tag where global_tag_id=%s", id))
+	if err != nil {
+		log.Println(err)
+		c.JSON(500, gin.H{
+			"server": -1,
+		})
+	} else {
+		tags := []Tag{}
+		for rows.Next() {
+			var tag Tag
+			rows.Scan(&tag.TagID, &tag.Activity, &tag.GlobalTagID)
+			tags = append(tags, tag)
+		}
+		c.JSON(200, tags)
+	}
+}
+
+func getAllUsers(c *gin.Context) {
+	rows, err := DB.Query("select * from users")
+	if err != nil {
+		log.Println(err)
+		c.JSON(500, gin.H{
+			"server": -1,
+		})
+	} else {
+		users := []User{}
+		for rows.Next() {
+			var usr User
+			rows.Scan(&usr.UserId, &usr.Name, &usr.Nickname, &usr.Rate, &usr.Description, &usr.Login, &usr.Password)
+			usr.Password = ""
+			users = append(users, usr)
+		}
+		c.JSON(200, users)
 	}
 }
